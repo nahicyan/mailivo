b5035b20a887a62f8f791ea569651bb134564c30
Cyan <nahicyan@protonmail.com>
2025-07-21 09:32:42 -0500

    Fix: Project Run & Create Project

--- app/src/hooks/useLandivoProperties.ts
+++ app/src/hooks/useLandivoProperties.ts
@@ -4,7 +4,7 @@ import { LandivoProperty } from '@/types/landivo';
 const serverURL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000';
 
 async function fetchLandivoProperties(): Promise<LandivoProperty[]> {
-  const response = await fetch(`${serverURL}/landivo/properties`, {
+  const response = await fetch(`${serverURL}/residency/allresd/`, {
     credentials: 'include',
   });
   
